21c1d64d08d53aae45b95c1245d0ed924790cbb7
Sofialav <cemeteryvixen@gmail.com>
2020-03-06 10:36:02 +0100

    post/message, parser added

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
+
 const app = express();
 const port = 4000;
+const db = {};
+db.messages = [];
+
+const parser = express.json();
+
+app.use(parser);
+app.post("/message", (req, res) => {
+  const { text } = req.body;
+  db.messages.push(text);
+  res.send(text);
+  console.log(db);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
