864156fe4da9f5e246d4e152b3ac3a43ea499e06
Sofialav <cemeteryvixen@gmail.com>
2020-03-06 15:43:38 +0100

    heroku scripts

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const Sse = require("json-sse");
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
 const db = {};
 db.messages = [];
 
+const corsMW = cors();
+app.use(corsMW);
 const parser = express.json();
 app.use(parser);
-
 const stream = new Sse();
 
 app.get("/stream", (req, res) => {
-  stream.updateInit(db.messages);
+  const action = {
+    type: "ALL_MESSAGES",
+    payload: db.messages
+  };
+  stream.updateInit(action);
   stream.init(req, res);
 });
 
@@ -20,7 +26,12 @@ app.post("/message", (req, res) => {
   const { text } = req.body;
   db.messages.push(text);
   res.send(text);
-  stream.send(text);
+
+  const action = {
+    type: "NEW_MESSAGE",
+    payload: text
+  };
+  stream.send(action);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
