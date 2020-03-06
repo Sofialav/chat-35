0eb370b99bce41343374618a011a10abad945eeb
Sofialav <cemeteryvixen@gmail.com>
2020-03-06 11:02:15 +0100

    get/stream with update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Sse = require("json-sse");
 
 const app = express();
 const port = 4000;
@@ -6,13 +7,20 @@ const db = {};
 db.messages = [];
 
 const parser = express.json();
-
 app.use(parser);
+
+const stream = new Sse();
+
+app.get("/stream", (req, res) => {
+  stream.updateInit(db.messages);
+  stream.init(req, res);
+});
+
 app.post("/message", (req, res) => {
   const { text } = req.body;
   db.messages.push(text);
   res.send(text);
-  console.log(db);
+  stream.send(text);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
